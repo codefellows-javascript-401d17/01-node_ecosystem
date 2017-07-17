1a66a64bb8f50f985ef31a4dd73749bb5ed8402d
ohjonah <ohjonah@gmail.com>
2017-07-17 15:15:07 -0700

    Adds README

--- lab-jonah/test/greet-test.js
+++ lab-jonah/test/greet-test.js
@@ -4,9 +4,9 @@ const greet = require('../lib/greet.js');
 const assert = require('assert');
 
 describe('Greet Module', function() {
-  it('should return Hello Jonah!', function() {
-    var result = greet('Jonah');
-    assert.ok(result === 'Hello Jonah!', 'not equal to Hello Jonah!');
+  it('should return Hello World!', function() {
+    var result = greet('World');
+    assert.ok(result === 'Hello World!', 'not equal to Hello World!');
   });
 
   it('should throw an invalid string error', function() {
