1088ca1c4f0df198758fec05aaa67ab5b25bce11
Allan Liebold <allan.liebold@gmail.com>
2017-07-17 14:41:17 -0700

    tests for non-string arguments in greet.js as non-number arguments in arithmetic.js

--- lab-allan/lib/arithmetic.js
+++ lab-allan/lib/arithmetic.js
@@ -3,11 +3,13 @@
 module.exports = exports = {};
 
 exports.add = function(first, second) {
-  if (isNaN(first) || isNaN(second)) throw new Error('not a number');
+  if (isNaN(first) || isNaN(second))
+    return null;
   return first + second;
 };
 
 exports.sub = function(first, second) {
-  if (isNaN(first) || isNaN(second)) throw new Error('not a number');
+  if (isNaN(first) || isNaN(second))
+    return null;
   return first - second;
 };
--- lab-allan/test/arithmetic-test.js
+++ lab-allan/test/arithmetic-test.js
@@ -9,11 +9,19 @@ describe('Arithmetic Module Test', function() {
       let result = arithmetic.add(3, 2);
       assert.ok(result === 5, 'not equal to 5');
     });
+    it('should return null', function() {
+      let result = arithmetic.add('word', 5);
+      assert.ok(result === null, 'null not returned');
+    });
   });
   describe('#sub', function() {
     it('should return 1', function() {
       let result = arithmetic.sub(3, 2);
       assert.ok(result === 1, 'not equal to 1');
+    });
+    it('should return null', function() {
+      let result = arithmetic.sub('word', 50);
+      assert.ok(result === null, 'null not returned');
     });
   });
 });
