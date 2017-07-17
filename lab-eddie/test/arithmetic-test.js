20a570b9fa8dfb4c2b654d0ff9c868f888f2b673
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-17 14:06:25 -0700

    Wrote test for arithmetic module
    Tests the add method for sum of both arguements, 2 valid arguements and NaN arguements
    Tests the sub method for range of both arguements, 2 valid arguements and NaN arguements

--- lab-eddie/test/arithmetic-test.js
+++ lab-eddie/test/arithmetic-test.js
@@ -1,4 +1,41 @@
 'use strict';
 
-const greet = require('../lib/arithmetic.js');
+const arithmetic = require('../lib/arithmetic.js');
 const assert = require('assert');
+
+describe('Arithmetic Module', function() {
+  describe('#add', function() {
+    it('Should return the sum of both arguements', function() {
+      let sum = arithmetic.add(4, 7);
+      assert (4 + 7 === sum, 'Answer not equal to 11');
+      sum = arithmetic.add(9, 5);
+      assert (5 + 9 === sum, 'Answer not equal to 14');
+    });
+    it('Should throw error when not given 2 arguements', function() {
+      assert.throw(arithmetic.add(9), 'Error not thrown');
+      assert.throw(arithmetic.add(), 'Error not thrown');
+    });
+    it('Should throw error when given a NaN arguement', function() {
+      assert.throw(arithmetic.add('a', 5), 'Error not thrown');
+      assert.throw(arithmetic.add(1, 'c'), 'Error not thrown');
+    });
+
+  });
+
+  describe('#sub', function() {
+    it('Should return the difference of both arguements', function() {
+      let sum = arithmetic.sub(4, 7);
+      assert (4 - 7 === sum, 'Answer not equal to 11');
+      sum = arithmetic.sub(9, 5);
+      assert (5 - 9 === sum, 'Answer not equal to 11');
+    });
+    it('Should throw error when not given 2 arguements', function() {
+      assert.throw(arithmetic.sub(9), 'Error not thrown');
+      assert.throw(arithmetic.sub(), 'Error not thrown');
+    });
+    it('Should throw error when given a NaN arguement', function() {
+      assert.throw(arithmetic.sub('a', 5), 'Error not thrown');
+      assert.throw(arithmetic.sub(1, 'c'), 'Error not thrown');
+    });
+  });
+});
--- lab-eddie/test/greet-test.js
+++ lab-eddie/test/greet-test.js
@@ -5,11 +5,15 @@ const assert = require('assert');
 
 describe('Greet Module', function() {
   describe('#sayHey', function() {
-    it('should return hey brian!', function() {
+    it('should return hey world!', function() {
       var result = greet('world');
       assert.ok(result === 'hello world!', 'Not equal to hello world!');
     });
-    it('should throw a missing name error', function() {
+    it('should return null!', function() {
+      var result = greet('world');
+      assert.ok(result === null, 'Not equal to null');
+    });
+    it('should throw a missing name error when no arguements are given', function() {
       assert.throws(function() {
         greet();
       }, 'error not thrown');
