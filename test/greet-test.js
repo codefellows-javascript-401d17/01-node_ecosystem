ec29c217fe125e084903b8b5b75b2291241cf62f
Loaye <jr.carstens00@gmail.com>
2017-07-18 11:02:52 -0700

    fixed indentations

--- test/greet-test.js
+++ test/greet-test.js
@@ -5,13 +5,13 @@ const assert = require('assert');
 
 describe('Greet Module', function(){
   describe('#greeting', function(){
-      it('should return hello world!', function(){
-        var result = greeting('world');
-        assert.ok(result === 'hello world!', 'not equal to hello world!');
-      });
-      it('should throw not a string error', function(){
-        var result = greeting(4);
-        assert.ok(result === null, 'error not thrown');
+    it('should return hello world!', function(){
+      var result = greeting('world');
+      assert.ok(result === 'hello world!', 'not equal to hello world!');
+    });
+    it('should throw not a string error', function(){
+      var result = greeting(4);
+      assert.ok(result === null, 'error not thrown');
     });
   });
 });
